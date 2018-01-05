46232766a7355e6d0285866c7272bf6aba1b4863
lmaddox <laurence.a.maddox@gmail.com>
2018-01-04 19:28:30 -0600

    meh

--- src/P2p/P2pNetworks.h
+++ src/P2p/P2pNetworks.h
@@ -1,13 +1,25 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
 namespace CryptoNote
 {
-  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 
-    0xc8, 0xec, 0x9d, 0x62, 0x82, 0x2c, 0xdb, 0xa5,
-    0x88, 0x47, 0xa4, 0xc9, 0xe7, 0x34, 0x27, 0xe6 
-  } };
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0xe3, 0x3b, 0x14, 0xf4, 0x34, 0x10, 0xbc, 0xb6, 0x89, 0x31, 0xfd, 0x46, 0x96, 0x27, 0xcd, 0x7d  } };
+
 }
+
